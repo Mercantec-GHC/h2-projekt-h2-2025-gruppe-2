baa0bf412cc4d559ed620cbe50fb9fca141774a6
Andreas <andreas3x3@protonmail.com>
2025-08-18 13:43:05 +0200

    Added a function to resolve roleId in role name. Added a function which allows the admin to change an accounts role.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using API.Data;
 using API.Service;
 using DomainModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace API.Controllers
 {
@@ -14,6 +15,7 @@ namespace API.Controllers
     {
         private readonly AppDBContext _context;
         private readonly JwtService _jwtService;
+        private readonly TimeService _timeService = new();
         private const int workFactor = 12;
 
         public UsersController(AppDBContext context, JwtService jwtService)
@@ -90,7 +92,7 @@ namespace API.Controllers
             if (userRole == null)
                 return BadRequest("Standard brugerrolle ikke fundet.");
             
-            DateTime utcNow = DateTime.UtcNow.AddHours(2);
+            DateTime utcNow = _timeService.GetCopenhagenTime();
             var user = new User
             {
                 Id = Guid.NewGuid().ToString(),
@@ -136,7 +138,7 @@ namespace API.Controllers
             if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.HashedPassword))
                 return Unauthorized("Forkert email eller adgangskode");
             
-            user.LastLogin = DateTime.UtcNow.AddHours(2);
+            user.LastLogin = _timeService.GetCopenhagenTime();
             await _context.SaveChangesAsync();
 
             // Generer JWT token josh
@@ -180,6 +182,34 @@ namespace API.Controllers
                 CreatedAt = user.CreatedAt,
                 Roles = user.Roles?.Name
             });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPatch("change/role")]
+        public async Task<IActionResult> ChangeRole(string id, string roleName)
+        {
+            User? user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return BadRequest("User was not found with the id: " + id);
+            
+            
+            Role? role = await _context.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
+            if (role == null)
+                return BadRequest("Role was not found: " + roleName);
+            
+            var beforeRoleId = user.RoleId;
+            user.RoleId = role.Id;
+            user.UpdatedAt = _timeService.GetCopenhagenTime();
+            var updatedUser = await _context.SaveChangesAsync();
+            if (updatedUser == 0)
+                return BadRequest("User was not updated");
+            
+            await _context.Entry(user).Reference(u => u.Roles).LoadAsync();
+            
+            if (user.Roles == null)
+                return BadRequest("Roles was not found for user: " + id);
+            
+            return Ok($"Role for user {id} got updated from {user.Roles.ResolveRoleId(beforeRoleId)} to {user.Roles.ResolveRoleId(user.RoleId)}");
         }
 
         private bool UserExists(string id)
