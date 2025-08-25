cc481f56a669f27def216f8c842d32f954253b42
Andreas <andreas3x3@protonmail.com>
2025-08-25 13:11:16 +0200

    You can now login, where the token is saved to local storage

--- Blazor/Services/APIService.User.cs
+++ Blazor/Services/APIService.User.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json;
+using DomainModels;
 
 namespace Blazor.Services;
 
@@ -31,6 +33,46 @@ public partial class APIService
         {
             Console.WriteLine(ex.Message);
             return (false, "Generel exception caught, please contact support:  " + ex.Message);
+        }
+    }
+
+    public async Task<(bool status, string msg, string token)> LoginUser(string email, string password)
+    {
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Users/login", new
+            {
+                email = email,
+                password = password
+            });
+
+            if (response.IsSuccessStatusCode)
+            {
+                var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
+                return (true, "Login successfull!", loginResponse.token);
+            }
+            else
+            {
+                var errorMsg = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Test: " + errorMsg);
+                return (false,
+                    "Failed to create new user! Code: " + response.StatusCode + ", " + errorMsg, "");
+            }
+        }
+        catch (NullReferenceException ex)
+        {
+            Console.WriteLine("Failed to parse JSON response, JSON is null: " + ex.Message);
+            return (false, "Failed to parse JSON response, JSON is null: " + ex.Message, "");      
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine("Failed to parse JSON response: " + ex.Message);
+            return (false, "Failed to parse JSON response: " + ex.Message, "");       
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Generel exception caught, please contact support:" + ex.Message);
+            return (false, "Generel exception caught, please contact support:  " + ex.Message, "");
         }
     }
 }
