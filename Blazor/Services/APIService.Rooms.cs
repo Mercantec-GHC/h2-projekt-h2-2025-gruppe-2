c25766882d44794b91d32572cb11144070705158
Okiron <73331721+ChrissieDev@users.noreply.github.com>
2025-08-27 10:11:51 +0200

    Made all-rooms page, with a new Rooms API service.

---
+++ Blazor/Services/APIService.Rooms.cs
@@ -0,0 +1,14 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using DomainModels;
+
+namespace Blazor.Services;
+
+public partial class APIService
+{
+    public async Task<List<Room>> GetRooms(CancellationToken ct = default)
+    {
+        var res = await _httpClient.GetFromJsonAsync<List<Room>>("api/Rooms", ct);
+        return res ?? new();
+    }
+}
